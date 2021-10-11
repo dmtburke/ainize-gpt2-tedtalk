9ea79fa94e7ec625e261d30cad0f7bfad4bc039d
Dalton Burke <dmtburke@gmail.com>
2021-10-11 02:46:49 -0600

    fixed errors with batching, and text length

--- app.py
+++ app.py
@@ -10,6 +10,29 @@ tokenizer = GPT2Tokenizer.from_pretrained("./GPT2-TedTalk")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
+requests_queue = Queue()    # request queue.
+BATCH_SIZE = 1              # max request size.
+CHECK_INTERVAL = 0.1
+
+def handle_requests_by_batch():
+    while True:
+        request_batch = []
+
+        while not (len(request_batch) >= BATCH_SIZE):
+            try:
+                request_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
+            except Empty:
+                continue
+
+            for requests in request_batch:
+                try:
+                    requests["output"] = make_story(requests['input'][0], requests['input'][1])
+
+                except Exception as e:
+                    requests["output"] = e
+
+
+handler = Thread(target=handle_requests_by_batch).start()
 
 def make_presentation(base_text, length):
     try:
@@ -17,15 +40,23 @@ def make_presentation(base_text, length):
         input_ids = tokenizer.encode(base_text, return_tensors='pt')
         # Both input and model must use the same device (cpu or gpu)
         input_ids = input_ids.to(device)
+
+        min_length = len(input_ids.tolist()[0])
+
+        length = length if length > 0 else 1
+
+        length += min_length
         # Generate prediction
         outputs = model.generate(input_ids, pad_token_id=50256,
                                  do_sample=True,
+                                 min_length=min_length,
                                  max_length=length,
                                  top_k=40,
                                  num_return_sequences=1)
         result = dict()
-        result["prediction"] = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return jsonify(result)
+        for idx, sample_output in enumerate(gen_ids):
+            result[0] = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
+        return result
 
     except Exception as e:
         print('Error occur in script generating!', e)
